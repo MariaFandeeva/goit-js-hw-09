060a45dd7310088b9975a765c07d985cd4b2b763
MariaFandeeva <mfandeeva@gmail.com>
2022-10-07 16:17:05 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+const btnEls = document.querySelectorAll('button');
 
+let timerId;
+
+btnStartEl.addEventListener('click', onBtnStart);
+btnStopEl.addEventListener('click', onBtnStop);
+
+function onBtnStart(event) {
+  btnStartEl.disabled = true;
+  btnStopEl.disabled = false;
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function onBtnStop(event) {
+  clearTimeout(timerId);
+  btnStopEl.disabled = true;
+  btnStartEl.disabled = false;
+}
+btnStartEl.removeEventListener('click', onBtnStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
